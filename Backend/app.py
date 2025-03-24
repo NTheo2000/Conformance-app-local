94c8cc921e789cf2d673c22c654f9dca9f2ff8dd
NTheo2000 <nikolastheo3@gmail.com>
2025-03-24 18:58:44 +0100

    csv to xes

--- Backend/app.py
+++ Backend/app.py
@@ -2,7 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 from process_mining.process_bpmn import parse_bpmn
-from process_mining.process_csv import parse_csv
+from process_mining.process_xes import parse_xes
+
 
 app = Flask(__name__)
 CORS(app)  # Allow frontend to communicate with backend
@@ -12,26 +13,27 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/upload', methods=['POST'])
 def upload_files():
-    """Handles BPMN and CSV file uploads."""
-    if 'bpmn' not in request.files or 'csv' not in request.files:
-        return jsonify({"error": "Both BPMN and CSV files are required"}), 400
+    """Handles BPMN and XES file uploads."""
+    if 'bpmn' not in request.files or 'xes' not in request.files:
+        return jsonify({"error": "Both BPMN and XES files are required"}), 400
     
     bpmn_file = request.files['bpmn']
-    csv_file = request.files['csv']
+    xes_file = request.files['xes']
 
     bpmn_path = os.path.join(UPLOAD_FOLDER, bpmn_file.filename)
-    csv_path = os.path.join(UPLOAD_FOLDER, csv_file.filename)
+    xes_path = os.path.join(UPLOAD_FOLDER, xes_file.filename)
 
     bpmn_file.save(bpmn_path)
-    csv_file.save(csv_path)
+    xes_file.save(xes_path)
 
     to_be_process = parse_bpmn(bpmn_path)
-    as_is_process = parse_csv(csv_path)
+    as_is_process = parse_xes(xes_path)
 
     return jsonify({
         "to_be_process": to_be_process,
         "as_is_process": as_is_process
     })
+
 
 if __name__ == '__main__':
     app.run(debug=True)
